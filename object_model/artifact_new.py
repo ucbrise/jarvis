5665e5ad198d8efdb158fcc14c436b2bac849046
Malhar Patel <malhar@berkeley.edu>
2018-01-29 08:22:53 -0800

    Added additional functionality in artifact_new. Still need to add the manifest information back and test

--- object_model/artifact_new.py
+++ object_model/artifact_new.py
@@ -50,27 +50,61 @@ def parallelPull(self, manifest={}):
 				else:
 					literals.append([self.xp_state.literalNameToObj[kee].v, ])
 			literalNames.append(kee)
-
+	
 	literals = list(itertools.product(*literals))
 
 	for i in range(numTrials):
 		dst = tmpexperiment + '/' + str(i)
 		copytree(os.getcwd(), dst, True) #TODO: Check if needed
 
-	ts = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-	self.xp_state.ray['literalNames'] = literalNames
+	ts = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') #TODO: Check if needed?
+	self.xp_state.ray['literalNames'] = literalNames #TODO: Check what the implication of this is
 
 	#TODO: Run Functions in Parallel 
 
-	#Tmp directory initialization
+	#May need to move this outside of the function?
+	@ray.remote
+	def helperChangeDir(dir_path, f, literals):
+		os.chdir(dir_path)
+		f(literals)
 
-	#Modularize into helper Ray function
+	remaining_ids = []
+	for i in range(numTrials):
+		#FIXME: Add check if number of combinations of literals == numTrials??
+		dir_path = tmpexperiment + '/' + str(i)
+		literals = list(map(lambda x: self.xp_state.literalNameToObj[x].v, lambdas[i][1]))
+		f = lambdas[i][0]
 
-	# for f, names in lambdas:
-	# 	literals = list(map(lambda x: self.xp_state.literalNameToObj[x].v, names))
-	# 	f(literals)
+		remaining_ids.append(helperChangeDir.remote(dir_path, f, literals))
 
-	for _ in range(numTrials):
-		
+	_, _ = ray.wait(remaining_ids, num_returns=numTrials)
 
+	#Results directory initialization
+	
+	if not os.path.isdir(self.xp_state.versioningDirectory):
+		os.mkdir(self.xp_state.versioningDirectory)
+
+	moveBackFlag = False
+
+	if os.path.exists(self.xp_state.versioningDirectory + '/' + self.xp_state.jarvisFile.split('.')[0]):
+		move(self.xp_state.versioningDirectory + '/' + self.xp_state.jarvisFile.split('.')[0] + '/.git', '/tmp/')
+		rmtree(self.xp_state.versioningDirectory + '/' + self.xp_state.jarvisFile.split('.')[0])
+		moveBackFlag = True
+
+	#TODO: Add in the manifest condition (L 275 - 313 and L 329-331)
+
+	#Move Files from isolated environments to the ~/jarvis.d directory
+	copytree(tmpexperiment, self.xp_state.versioningDirectory + '/' + self.xp_state.jarvisFile.split('.')[0])
+
+	os.chdir(self.xp_state.versioningDirectory + '/' + self.xp_state.jarvisFile.split('.')[0])
+	if moveBackFlag:
+		move('/tmp/.git', self.xp_state.versioningDirectory + '/' + self.xp_state.jarvisFile.split('.')[0])
+		repo = git.Repo(os.getcwd())
+		repo.git.add(A=True)
+		repo.index.commit('incremental commit')
+	else:
+		repo = git.Repo.init(os.getcwd())
+		repo.git.add(A=True)
+		repo.index.commit('initial commit')
+	os.chdir(original_dir)
 
